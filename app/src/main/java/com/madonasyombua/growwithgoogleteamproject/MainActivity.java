2e898a383cfb58eafc38bc2d5ec8e948bc5dfb6d
Madona Syombua <syombuamadona@gmail.com>
2018-02-09 09:33:27 -0600

    Update MainActivity.java

--- app/src/main/java/com/madonasyombua/growwithgoogleteamproject/MainActivity.java
+++ app/src/main/java/com/madonasyombua/growwithgoogleteamproject/MainActivity.java
@@ -9,42 +9,11 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final String TAG = "LoginActivity";
-    EditText emailEnter;
-    EditText passwordEnter;
-    Button loginButton;
-    TextView signupLink;
+  
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-// EditText and Button Id on the Layout I will comment them for now then we can connect with the ids
-       /*
-        emailEnter=findViewById(R.id...);
-        passwordEnter=findViewById(R.id...);
-        loginButton=findViewById(R.id...);
-        signupLink=findViewById(R.id...);*/
 
-
-        loginButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //looking at creating a login method Login(); to help with validation
-            }
-        });
-
-        signupLink.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                //start the sign up Activity.
-            }
-        });
-    }
-
-
-    public void Login(){
-        //  tag
-        //looking at On complete call either onLoginSuccess or onLoginFailed
     }
 }
