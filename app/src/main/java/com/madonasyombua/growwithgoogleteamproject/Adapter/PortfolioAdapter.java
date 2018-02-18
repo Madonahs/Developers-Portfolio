287c2bf79ff27407cf4295762aed151c53625e02
Jantz <jantzillac.93@gmail.com>
2018-02-18 16:20:08 -0600

    Created RecyclerView for Portfolio Items

---
+++ app/src/main/java/com/madonasyombua/growwithgoogleteamproject/Adapter/PortfolioAdapter.java
@@ -0,0 +1,8 @@
+package com.madonasyombua.growwithgoogleteamproject.Adapter;
+
+/**
+ * Created by jantz on 2/18/2018.
+ */
+
+public class PortfolioAdapter {
+}
---
+++ app/src/main/java/com/madonasyombua/growwithgoogleteamproject/models/Portfolio.java
@@ -0,0 +1,8 @@
+package com.madonasyombua.growwithgoogleteamproject.models;
+
+/**
+ * Created by jantz on 2/18/2018.
+ */
+
+public class Portfolio {
+}
