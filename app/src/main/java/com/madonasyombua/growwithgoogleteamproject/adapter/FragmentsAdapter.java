1b773a3a961e1b80705be2bfdaecd645dc1c756c
Maher SOUA <mahersoua@gmail.com>
2018-02-18 22:59:32 +0100

    Rename package, use always lower case to name package

--- app/src/main/java/com/madonasyombua/growwithgoogleteamproject/adapter/FragmentsAdapter.java
+++ app/src/main/java/com/madonasyombua/growwithgoogleteamproject/adapter/FragmentsAdapter.java
@@ -1,4 +1,4 @@
-package com.madonasyombua.growwithgoogleteamproject.Adapter;
+package com.madonasyombua.growwithgoogleteamproject.adapter;
 
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
