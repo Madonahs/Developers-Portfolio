47b2a79d37cf6e636b16dd7674756ada8b6290eb
Cleverchuk <chubiyke@gmail>
2018-02-23 00:41:26 -0500

    fix error

--- app/src/main/java/com/madonasyombua/growwithgoogleteamproject/ui/fragment/ProfileFragment.java
+++ app/src/main/java/com/madonasyombua/growwithgoogleteamproject/ui/fragment/ProfileFragment.java
@@ -105,32 +105,18 @@ public class ProfileFragment extends Fragment {
     }
 
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
-    }
     private void setStatus(boolean online){
         if(online)
         {
-           // mBinding.status.setCompoundDrawablesWithIntrinsicBounds(getResources()
-         //                   .getDrawable(R.drawable.ic_online),null,null,null);
-        //    mBinding.status.setText(getString(R.string.online));
+            mBinding.status.setCompoundDrawablesWithIntrinsicBounds(getResources()
+                            .getDrawable(R.drawable.ic_online),null,null,null);
+            mBinding.status.setText(getString(R.string.online));
         }
         else
         {
-        //    mBinding.status.setCompoundDrawablesWithIntrinsicBounds(getResources()
-        //            .getDrawable(R.drawable.ic_offline),null,null,null);
-      //      mBinding.status.setText(getString(R.string.offline));
+            mBinding.status.setCompoundDrawablesWithIntrinsicBounds(getResources()
+                    .getDrawable(R.drawable.ic_offline),null,null,null);
+            mBinding.status.setText(getString(R.string.offline));
         }
 
     }
