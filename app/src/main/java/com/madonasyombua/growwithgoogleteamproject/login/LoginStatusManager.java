d393cf7934a1bf7672afc92c27988e08caa5e331
Maher SOUA <mahersoua@gmail.com>
2018-02-27 23:41:13 +0100

    Extract static string to string resource

--- app/src/main/java/com/madonasyombua/growwithgoogleteamproject/login/LoginStatusManager.java
+++ app/src/main/java/com/madonasyombua/growwithgoogleteamproject/login/LoginStatusManager.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 
+import com.madonasyombua.growwithgoogleteamproject.R;
+
 /**
  * Created by mahersoua on 27/02/2018.
  */
@@ -12,13 +14,14 @@ public class LoginStatusManager {
     public static void storeLoginStatus(Context context){
         SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
         SharedPreferences.Editor editor = sharedPreferences.edit();
-        editor.putBoolean("logged", true);
+        editor.putBoolean(context.getString(R.string.pref_login_status_key), true);
         editor.apply();
     }
 
     public static boolean getLoginStatus(Context context){
         SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
         SharedPreferences.Editor editor = sharedPreferences.edit();
-        return sharedPreferences.getBoolean("logged", false);
+        return sharedPreferences.getBoolean(context.getString(R.string.pref_login_status_key),
+                context.getResources().getBoolean(R.bool.pref_login_status_default_key));
     }
 }
