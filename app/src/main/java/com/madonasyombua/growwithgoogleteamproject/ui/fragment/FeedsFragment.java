0a840b6f17d9d8ab6729b45a1a829b13540bfda0
Madonahs <syombuamadona@gmail.com>
2018-03-02 21:51:26 -0600

    feeds-layout

--- app/src/main/java/com/madonasyombua/growwithgoogleteamproject/ui/fragment/FeedsFragment.java
+++ app/src/main/java/com/madonasyombua/growwithgoogleteamproject/ui/fragment/FeedsFragment.java
@@ -1,25 +1,32 @@
 package com.madonasyombua.growwithgoogleteamproject.ui.fragment;
 
 import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.Loader;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.madonasyombua.growwithgoogleteamproject.FeedsActivity;
 import com.madonasyombua.growwithgoogleteamproject.interfaces.OnFragmentInteractionListener;
 import com.madonasyombua.growwithgoogleteamproject.R;
 
 /**
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
- * {@link FeedsFragment.OnFragmentInteractionListener} interface
  * to handle interaction events.
  * Use the {@link FeedsFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class FeedsFragment extends Fragment {
+public class FeedsFragment extends Fragment implements LoaderManager.LoaderCallbacks<Cursor>{
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -28,7 +35,7 @@ public class FeedsFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
-
+    private View view;
     private OnFragmentInteractionListener mListener;
 
     public FeedsFragment() {
@@ -65,8 +72,37 @@ public class FeedsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_feeds, container, false);
+
+        view = inflater.inflate(R.layout.fragment_feeds, container, false);
+        //Home ToolBar
+
+       /* Toolbar toolbar =  view.findViewById(R.id.feedsToolbar);
+        ((AppCompatActivity) getActivity()).setSupportActionBar(toolbar);
+        if (((AppCompatActivity) getActivity()).getSupportActionBar() != null)
+            ((AppCompatActivity) getActivity()).getSupportActionBar().setTitle(R.string.home);*/
+
+
+
+         //starting the float button
+        FloatingActionButton addFeeds =  view.findViewById(R.id.add_feeds);
+        if ( addFeeds != null)
+            addFeeds.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    openFeedsActivity(view);
+                }
+            });
+        getLoaderManager().initLoader(0, null, this);
+
+
+
+        return view;
+    }
+//my method to open the FeedsActivity
+    private void openFeedsActivity(@SuppressWarnings("unused") View view) {
+        Intent intent = new Intent(this.getActivity(), FeedsActivity.class);
+        startActivity(intent);
+
     }
 
     // TODO: Rename method, update argument and hook method into UI event
@@ -93,4 +129,18 @@ public class FeedsFragment extends Fragment {
         mListener = null;
     }
 
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        return null;
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+
+    }
 }
