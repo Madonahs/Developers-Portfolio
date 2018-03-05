245be8c0cbd2aae61bcbaef6680e902af4ef2ddc
Madonahs <syombuamadona@gmail.com>
2018-03-05 07:10:02 -0600

    changed profile colordark to primary

--- app/src/main/java/com/madonasyombua/growwithgoogleteamproject/InterestActivity.java
+++ app/src/main/java/com/madonasyombua/growwithgoogleteamproject/InterestActivity.java
@@ -9,5 +9,7 @@ public class InterestActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_interest);
+
+
     }
 }
