126e4005c6d903914218aeba1d98b7bbf5fcb5f3
kiduel <kiduel@gmail.com>
2018-03-12 19:56:47 -0700

    Theme (#99)
    * theme part one
    
    * theme part two
    
    * change color

---
+++ app/src/main/java/com/madonasyombua/growwithgoogleteamproject/ui/SharedPref.java
@@ -0,0 +1,28 @@
+package com.madonasyombua.growwithgoogleteamproject.ui;
+
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+/**
+ * Created by kidus11 on 3/8/18.
+ */
+
+public class SharedPref {
+    SharedPreferences mySharedPref ;
+    public SharedPref(Context context) {
+        mySharedPref = context.getSharedPreferences("filename",Context.MODE_PRIVATE);
+    }
+    // This will save the night state - True or False
+    public void setNightModeState(Boolean state) {
+        SharedPreferences.Editor editor = mySharedPref.edit();
+        editor.putBoolean("enable_dark_mode",state);
+        editor.apply();
+    }
+    // This will load the Night State
+    public Boolean loadNightModeState (){
+        Boolean state = mySharedPref.getBoolean("enable_dark_mode",false);
+        return  state;
+    }
+}
+
