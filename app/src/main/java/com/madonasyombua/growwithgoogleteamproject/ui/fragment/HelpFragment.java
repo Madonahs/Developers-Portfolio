d9c667f22fc7f9b989450d79fd81e4f7543f4a0b
Madonahs <syombuamadona@gmail.com>
2018-04-03 19:30:20 -0500

    cleaning code

--- app/src/main/java/com/madonasyombua/growwithgoogleteamproject/ui/fragment/HelpFragment.java
+++ app/src/main/java/com/madonasyombua/growwithgoogleteamproject/ui/fragment/HelpFragment.java
@@ -14,14 +14,10 @@
  */
 package com.madonasyombua.growwithgoogleteamproject.ui.fragment;
 
-import android.content.Context;
+
 import android.net.Uri;
-import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import com.madonasyombua.growwithgoogleteamproject.R;
+
 
 
 public class HelpFragment extends Fragment {
